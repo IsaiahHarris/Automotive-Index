297688494843ad93ad997be2f929b23b16297036
Isaiah Harris <harrisfambam@gmail.com>
2018-07-31 11:31:03 -1000

    select gtr

--- indexing.sql
+++ indexing.sql
@@ -19,4 +19,12 @@ OWNER = indexed_cars_user;
 \i scripts/car_model_data.sql
 \i scripts/car_model_data.sql
 
+\timing
 
+SELECT DISTINCT ON (make_title) make_title FROM car_models
+WHERE make_code = 'LAM';
+
+\timing
+
+SELECT DISTINCT ON (model_title) model_title FROM car_models
+WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
